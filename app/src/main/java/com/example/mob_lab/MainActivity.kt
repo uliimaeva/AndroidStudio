a841815d9a78dccf02a6452d76d30db4d677411b
ulia <i.julka14@yandex.ru>
2022-10-29 07:55:59 +0300

    Init

---
+++ app/src/main/java/com/example/mob_lab/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.mob_lab
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
